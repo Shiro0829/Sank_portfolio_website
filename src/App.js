a63086d44716284229857e9ed2624df8e2ca2fa0
KattelSank <sankalpa123kattel@gmail.com>
2025-06-09 12:56:55 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          This is Sank portfolio message 
+        </p>
+        <p>
+          Learning Github
+        </p>
+    </div>
+  );
+}
+
+export default App;
